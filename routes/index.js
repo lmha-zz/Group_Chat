c3e3636ab9aa8382ad38e9b1545d6ec12ed8057d
Lisa Ha <lisamayha@gmail.com>
2014-08-01 18:12:32 -0700

    added new user joined announcement

--- routes/index.js
+++ routes/index.js
@@ -11,15 +11,19 @@ module.exports = function Route(app) {
 			req.io.emit('new_name');
 		} else {
 			req.io.emit('welcome_back', { name: req.session.name, message: "You've visited before! Your name was, "+req.session.name+", last time! We'll keep using that name!" } );
-			app.io.broadcast('new_user', req.session.name );
+			req.io.broadcast('new_user', req.session.name );
+			existing_messages.push("<p><i><b>"+req.session.name+" has joined the Conversation Board.</b></i></p>");
 		}
 		req.io.emit('existing_messages', existing_messages);
 	})
 
 	app.io.route('typed_name', function(req, res) {
 		req.session.name = req.data;
-		req.session.save();
-		app.io.broadcast('new_user', req.session.name );
+		req.session.save( function() {
+			app.io.broadcast('new_user', req.session.name );
+			existing_messages.push("<p><i><b>"+req.session.name+" has joined the Conversation Board.</b></i></p>");
+		});
+
 	})
 	
 	app.io.route('new_message', function(req, res) {
